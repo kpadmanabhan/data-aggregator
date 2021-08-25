556bb56b0bea92689241693ad310b854121cf080
kpadmanabhan <k.padmanabhan.1@elsevier.com>
2021-08-25 09:59:59 +0200

    adding missing unsaved changes

--- DataAggregator/Services/PhotoAlbumService.cs
+++ DataAggregator/Services/PhotoAlbumService.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using DataAggregator.Model;
 using Microsoft.Extensions.Logging;
